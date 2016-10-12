0507c134073905d319bdf4d9df9e71b55daabf02
warlyware <danward@gmail.com>
2016-10-11 20:16:39 -0400

    Add play pause mechanic

--- src/app/audio/audio.controller.js
+++ src/app/audio/audio.controller.js
@@ -11,10 +11,30 @@
         function AudioController(ngAudio) {
             var vm = this;
             var currentTrack;
+            var isPlaying = false;
 
             vm.playPause = function() {
+                if (angular.isDefined(currentTrack)) {
+                    isPlaying ? pauseTrack() : playTrack();
+                } else {
+                    loadTrack();
+                }
+            }
+
+            function loadTrack() {
                 currentTrack = ngAudio.load("https://ia600406.us.archive.org/23/items/PeterHernandezPodcastAudioPlaceholder/AudioPlaceholder.mp3");
+                playTrack();
+                isPlaying = true;
+            }
+
+            function playTrack() {
                 currentTrack.play();
+                isPlaying = true;
+            }
+
+            function pauseTrack() {
+                currentTrack.pause();
+                isPlaying = false;
             }
 
             return vm;
