ff0078e2b76c26bf7120f67dc3fa75fff3a3c941
warlyware <danward@gmail.com>
2016-10-11 20:39:36 -0400

    Swap hard coded audio player list with ng-repeat over dummy tracks

--- src/app/audio/audio.controller.js
+++ src/app/audio/audio.controller.js
@@ -13,6 +13,15 @@
             var currentTrack;
             var isPlaying = false;
 
+            vm.tracks = [{
+                title: 'Track 1',
+                fileName: 'https://ia600406.us.archive.org/23/items/PeterHernandezPodcastAudioPlaceholder/AudioPlaceholder.mp3'
+            },
+            {
+                title: 'Track 2',
+                fileName: 'https://ia600406.us.archive.org/23/items/PeterHernandezPodcastAudioPlaceholder/AudioPlaceholder.mp3'
+            }];
+
             vm.playPause = function() {
                 if (angular.isDefined(currentTrack)) {
                     isPlaying ? pauseTrack() : playTrack();
@@ -21,8 +30,14 @@
                 }
             }
 
-            function loadTrack() {
-                currentTrack = ngAudio.load("https://ia600406.us.archive.org/23/items/PeterHernandezPodcastAudioPlaceholder/AudioPlaceholder.mp3");
+            vm.loadTrack = function(track) {
+                console.log('loading ' + track);
+                loadTrack(track);
+            }
+
+            function loadTrack(track) {
+                stopCurrentTrack();
+                currentTrack = ngAudio.load(track);
                 playTrack();
                 isPlaying = true;
             }
@@ -35,6 +50,12 @@
             function pauseTrack() {
                 currentTrack.pause();
                 isPlaying = false;
+            }
+
+            function stopCurrentTrack() {
+                if (angular.isDefined(currentTrack)) {
+                    currentTrack.stop();
+                }
             }
 
             return vm;
